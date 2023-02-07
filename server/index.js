ceed09fcf66165665403a99c52c0a3b74b0febf3
gabe19p <g19purselley@gmail.com>
2023-02-07 11:41:12 +0900

    u/d mongo conn string

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 /**
  * Require statements
  */
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
 
 const app = express(); // Express variable.
 
@@ -11,26 +11,32 @@ const app = express(); // Express variable.
  * App configurations.
  */
 app.use(express.json());
-app.use(express.urlencoded({'extended': true}));
-app.use(express.static(path.join(__dirname, '../dist/bcrs')));
-app.use('/', express.static(path.join(__dirname, '../dist/bcrs')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../dist/bcrs")));
+app.use("/", express.static(path.join(__dirname, "../dist/bcrs")));
 
 // default server port value.
 const PORT = process.env.PORT || 3000;
 
 // TODO: This line will be replaced with your database connection string (including username/password).
-const CONN = 'mongodb+srv://superadmin:s3cret@cluster0-lujih.mongodb.net/bcrs?retryWrites=true&w=majority';
+const CONN =
+  "mongodb+srv://bcrsAdmin:s3cret@buwebdev-cluster-1.078ar.mongodb.net/bcrs?retryWrites=true&w=majority";
+
+// mongodb+srv://bcrsAdmin:s3cret@buwebdev-cluster-1.078ar.mongodb.net/bcrs?retryWrites=true&w=majority
 
 /**
  * Database connection.
  */
-mongoose.connect(CONN).then(() => {
-  console.log('Connection to the database was successful');
-}).catch(err => {
-  console.log('MongoDB Error: ' + err.message);
-});
+mongoose
+  .connect(CONN)
+  .then(() => {
+    console.log("Connection to the database was successful");
+  })
+  .catch((err) => {
+    console.log("MongoDB Error: " + err.message);
+  });
 
 // Wire-up the Express server.
 app.listen(PORT, () => {
-  console.log('Application started and listening on PORT: ' + PORT);
-})
+  console.log("Application started and listening on PORT: " + PORT);
+});
