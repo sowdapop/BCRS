b5c19c5f2578c4c9cf0c474beb40af931607b471
gabe19p <g19purselley@gmail.com>
2023-02-13 21:26:55 +0900

    fixed re-direct after fail auth

--- src/app/shared/auth.guard.ts
+++ src/app/shared/auth.guard.ts
@@ -1,4 +1,3 @@
-import { CookieService } from 'ngx-cookie-service';
 /**
  * Title:  Bob's Computer Repair Shop
  * Authors: Danial Purselley, William Watlington, Kayla McDanel
@@ -6,26 +5,31 @@ import { CookieService } from 'ngx-cookie-service';
  * Description: A web application for a computer repair shop
  */
 
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from "@angular/router";
+import { Observable } from "rxjs";
+import { CookieService } from "ngx-cookie-service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthGuard implements CanActivate {
-  
-  constructor(private router: Router, private cookieService: CookieService) { }
+  constructor(private router: Router, private cookieService: CookieService) {}
 
-  canActivate(
-    route: ActivatedRouteSnapshot,state: RouterStateSnapshot) {
-      const isAuthenticated = this.cookieService.get('sessionuser');
-    
-      if (isAuthenticated) {
-        return true;
-      } else {
-        this.router.navigate(['/']);
-        return false;
-      }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const isAuthenticated = this.cookieService.get("sessionuser");
+
+    if (isAuthenticated) {
+      return true;
+    } else {
+      this.router.navigate(["/session/login"]);
+      return false;
+    }
   }
 }
