387b18c5fb8d64ee8f05ff503bf3d5a8941532ff
gabe19p <g19purselley@gmail.com>
2023-02-16 12:04:47 +0900

    added attribution

--- src/app/shared/error.interceptor.ts
+++ src/app/shared/error.interceptor.ts
@@ -1,3 +1,10 @@
+/**
+ * Title:  Bob's Computer Repair Shop
+ * Authors: Danial Purselley, William Watlington, Kayla McDanel
+ * Date: 16 Feb 2023
+ * Description: A web application for a computer repair shop
+ */
+
 import { Injectable } from "@angular/core";
 import {
   HttpRequest,
